290c22dd62aea6296567a462bfbd7f732ed3e4e4
nicoya505 <figueroablanco92@gmail.com>
2023-08-02 16:41:07 +0200

    se creó la lista de criptomonedas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-
-
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
   
+  const[ criptos, setCriptos] = useState()
+
+  useEffect( ()=>{
+    fetch(`${API_URL}assets`)
+    .then( (response) => response.json())
+    .then( (data) =>{
+      setCriptos( data.data)
+    })
+    .catch( () => console.error("Error: la conexion falló"))
+
+  },[])
+
+  if( !criptos) return <span>Cargando...</span>
 
   return (
     <>
-      <h1>Bienvenido a mi proyecto con react</h1>
+      <h1>Listado de criptomonedas</h1>
+
+      <ul>
+        {criptos.map( ({id,name, priceUsd}) => (
+          <li key={id}>{name} - {priceUsd}</li>
+        ))}
+      </ul>
     </>
   )
 }
